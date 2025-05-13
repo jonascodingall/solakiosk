335e75bcdd41c90dc9f06f90c4d243ea63a9e4e8
jonas <jonas.coding.all@gmail.com>
2025-05-13 02:22:11 +0200

    added pb type-gen

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type Pocketbase, { AuthModel } from 'pocketbase';
+import type { TypedPocketBase } from '$lib/pocketbase-types';
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			pb: import('pocketbase').default;
-			user: import('pocketbase').default['authStore']['record'];
+			pb: TypedPocketBase;
+			user: AuthModel;
 		}
 		// interface PageData {}
 		// interface PageState {}
