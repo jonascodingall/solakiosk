898227ba3e949b074e841f711ace681a68a5d269
jonas <jonas.coding.all@gmail.com>
2025-05-13 02:18:20 +0200

    added basic pocketbase

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { createInstance } from '$lib/pocketbase';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
@@ -7,5 +8,27 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return new Response(null, { status: 204 }); // Return empty response with 204 No Content
 	}
 
-	return await resolve(event);
+	const pb = createInstance();
+
+	// load the store data from the request cookie string
+	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+	try {
+		// get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+		if (pb.authStore.isValid) {
+			await pb.collection('users').authRefresh();
+		}
+	} catch (_) {
+		// clear the auth store on failed refresh
+		pb.authStore.clear();
+	}
+
+	event.locals.pb = pb;
+	event.locals.user = pb.authStore.record;
+
+	const response = await resolve(event);
+
+	// send back the default 'pb_auth' cookie to the client with the latest store state
+	response.headers.set('set-cookie', pb.authStore.exportToCookie({ httpOnly: false }));
+
+	return response;
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+
+	import { browser } from '$app/environment';
+	import { applyAction, enhance } from '$app/forms';
+	import { setUserContext } from '$lib/contexts/user';
+	import { pb } from '$lib/pocketbase';
+	import { onDestroy, type Snippet } from 'svelte';
+	import { writable } from 'svelte/store';
+	import type { PageData } from './$types';
+
+	interface Props {
+		data: PageData;
+		children?: Snippet;
+	}
+
+	let { data, children }: Props = $props();
+
+	// Initialize user store
+	const user = writable(data.user);
+	setUserContext(user);
+
+	if (browser) {
+		// Load user from cookie (client-side only)
+		pb.authStore.loadFromCookie(document.cookie);
+
+		// Update user store when auth store changes
+		const unsubscribe = pb.authStore.onChange(() => {
+			user.set(pb.authStore.record);
+			document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
+		}, true);
+		onDestroy(unsubscribe);
+	}
 </script>
 
-{@render children()}
+{@render children?.()}
